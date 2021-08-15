1ee84e13827b8ef17bb6bef6d0632e519943e481
Владик Кононович <55635774+vladkk345@users.noreply.github.com>
2021-08-15 22:27:16 +0300

    Add files via upload

---
+++ Prog11.cpp
@@ -0,0 +1,61 @@
+﻿// Prog11.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <Windows.h>
+
+#define ROWS  3
+#define COLS  5
+
+//----------------------------------------------------------------------------
+void Source_values(int ar[][5], int rows);
+void Double_values(int ar[][5], int rows);
+//----------------------------------------------------------------------------
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int mass[ROWS][COLS] = { { 1, 1, 1, 1, 1 },
+						     { 2, 2, 2, 2, 2 },
+						     { 3, 3, 3, 3, 3 } };
+
+	Source_values(mass, 3);
+	Double_values(mass, 3);
+
+	return 0;
+}
+
+//----------------------------------------------------------------------------
+
+void Source_values(int ar[][5], int rows)
+{
+	printf("Исходный массив\n");
+
+	int i, j = 0;
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			printf("%d ", ar[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void Double_values(int ar[][5], int rows)
+{
+	printf("Массив с удвоенными значениями\n");
+
+	int i, j = 0;
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			printf("%d ", ar[i][j]*2);
+		}
+		printf("\n");
+	}
+}
